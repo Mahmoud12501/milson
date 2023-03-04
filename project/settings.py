8c5065e3640884aed1205dbfae4df8d82ccc1f7e
Mahmoud12501 <mahmoudmuhmmed2001@gmail.com>
2023-03-04 20:02:05 +0200

    deploy 2 change static root

--- project/settings.py
+++ project/settings.py
@@ -139,7 +139,7 @@ STATICFILES_DIRS = [
     '/var/www/static/',
 ]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
